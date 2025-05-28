8703fc3e13c84dae55bc71c742357eebdcf8eec7
장연우 <jangyw0927@naver.com>
2025-05-28 13:03:07 +0900

    // artchat 라우트 구분

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# FastAPI 서버를 실행하고, 위에서 만든 라우터를 등록하는 코드
+
+# main.py
 from fastapi import FastAPI
-from app.routes import router
+from app.routes import artchat
 
+# FastAPI 애플리케이션 생성
 app = FastAPI()
-app.include_router(router)
 
-@app.get("/")
-def root():
-    return {"message": "Hello, Artrithm"}
+# artchat 관련 라우터 등록 (URL prefix: /api/artchat)
+app.include_router(artchat.router, prefix="/api/artchat")
