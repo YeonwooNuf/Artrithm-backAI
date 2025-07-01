d009b045b6dde1ee5a33ba0a87b5c8aef035b830
장연우 <jangyw0927@naver.com>
2025-07-01 15:55:56 +0900

    // 추천 기능 api 라우터 등록

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-# FastAPI 서버를 실행하고, 위에서 만든 라우터를 등록하는 코드
-
-# main.py
 from fastapi import FastAPI
-from app.routes import artchat
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import artchat                    # 작품 PDF 챗봇 라우터
+from app.routes import recommend_faiss
 
-# FastAPI 애플리케이션 생성
+# ✅ FastAPI 애플리케이션 인스턴스 생성
 app = FastAPI()
 
-# artchat 관련 라우터 등록 (URL prefix: /api/artchat)
-app.include_router(artchat.router, prefix="/api/artchat")
+# ✅ CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # 프론트 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ✅ 라우터 등록
+
+app.include_router(artchat.router, prefix="/api/artchat")                 # 작품 PDF 챗봇
+
+app.include_router(recommend_faiss.router, prefix="/api")
